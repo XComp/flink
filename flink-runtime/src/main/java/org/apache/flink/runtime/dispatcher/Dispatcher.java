a76cddacba0d81304dfd9fddd35ffe85cd6e4b94
Matthias Pohl <matthias@ververica.com>
2021-12-15 15:54:14 +0100

    [FLINK-11813] Refactors Dispatcher constructor signature

--- flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/Dispatcher.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/Dispatcher.java
@@ -161,21 +161,64 @@ public abstract class Dispatcher extends PermanentlyFencedRpcEndpoint<Dispatcher
             DispatcherBootstrapFactory dispatcherBootstrapFactory,
             DispatcherServices dispatcherServices)
             throws Exception {
-        super(rpcService, RpcServiceUtils.createRandomName(DISPATCHER_NAME), fencingToken);
-        checkNotNull(dispatcherServices);
+        this(
+                rpcService,
+                fencingToken,
+                recoveredJobs,
+                globallyTerminatedJobs,
+                dispatcherServices.getConfiguration(),
+                dispatcherServices.getHighAvailabilityServices(),
+                dispatcherServices.getResourceManagerGatewayRetriever(),
+                dispatcherServices.getHeartbeatServices(),
+                dispatcherServices.getBlobServer(),
+                dispatcherServices.getFatalErrorHandler(),
+                dispatcherServices.getJobGraphWriter(),
+                dispatcherServices.getJobResultStore(),
+                dispatcherServices.getJobManagerMetricGroup(),
+                dispatcherServices.getMetricQueryServiceAddress(),
+                dispatcherServices.getIoExecutor(),
+                dispatcherServices.getHistoryServerArchivist(),
+                dispatcherServices.getArchivedExecutionGraphStore(),
+                dispatcherServices.getJobManagerRunnerFactory(),
+                dispatcherBootstrapFactory,
+                dispatcherServices.getOperationCaches());
+    }
 
-        this.configuration = dispatcherServices.getConfiguration();
-        this.highAvailabilityServices = dispatcherServices.getHighAvailabilityServices();
-        this.resourceManagerGatewayRetriever =
-                dispatcherServices.getResourceManagerGatewayRetriever();
-        this.heartbeatServices = dispatcherServices.getHeartbeatServices();
-        this.blobServer = dispatcherServices.getBlobServer();
-        this.fatalErrorHandler = dispatcherServices.getFatalErrorHandler();
-        this.jobGraphWriter = dispatcherServices.getJobGraphWriter();
-        this.jobResultStore = dispatcherServices.getJobResultStore();
-        this.jobManagerMetricGroup = dispatcherServices.getJobManagerMetricGroup();
-        this.metricServiceQueryAddress = dispatcherServices.getMetricQueryServiceAddress();
-        this.ioExecutor = dispatcherServices.getIoExecutor();
+    private Dispatcher(
+            RpcService rpcService,
+            DispatcherId fencingToken,
+            Collection<JobGraph> recoveredJobs,
+            Collection<JobResult> globallyTerminatedJobs,
+            Configuration configuration,
+            HighAvailabilityServices highAvailabilityServices,
+            GatewayRetriever<ResourceManagerGateway> resourceManagerGatewayRetriever,
+            HeartbeatServices heartbeatServices,
+            BlobServer blobServer,
+            FatalErrorHandler fatalErrorHandler,
+            JobGraphWriter jobGraphWriter,
+            JobResultStore jobResultStore,
+            JobManagerMetricGroup jobManagerMetricGroup,
+            String metricServiceQueryAddress,
+            Executor ioExecutor,
+            HistoryServerArchivist historyServerArchivist,
+            ExecutionGraphInfoStore executionGraphInfoStore,
+            JobManagerRunnerFactory jobManagerRunnerFactory,
+            DispatcherBootstrapFactory dispatcherBootstrapFactory,
+            DispatcherOperationCaches dispatcherOperationCaches)
+            throws Exception {
+        super(rpcService, RpcServiceUtils.createRandomName(DISPATCHER_NAME), fencingToken);
+
+        this.configuration = checkNotNull(configuration);
+        this.highAvailabilityServices = checkNotNull(highAvailabilityServices);
+        this.resourceManagerGatewayRetriever = checkNotNull(resourceManagerGatewayRetriever);
+        this.heartbeatServices = checkNotNull(heartbeatServices);
+        this.blobServer = checkNotNull(blobServer);
+        this.fatalErrorHandler = checkNotNull(fatalErrorHandler);
+        this.jobGraphWriter = checkNotNull(jobGraphWriter);
+        this.jobResultStore = checkNotNull(jobResultStore);
+        this.jobManagerMetricGroup = checkNotNull(jobManagerMetricGroup);
+        this.metricServiceQueryAddress = checkNotNull(metricServiceQueryAddress);
+        this.ioExecutor = checkNotNull(ioExecutor);
 
         this.jobManagerSharedServices =
                 JobManagerSharedServices.fromConfiguration(
@@ -183,11 +226,11 @@ public abstract class Dispatcher extends PermanentlyFencedRpcEndpoint<Dispatcher
 
         jobManagerRunnerRegistry = new JobManagerRunnerRegistry(16);
 
-        this.historyServerArchivist = dispatcherServices.getHistoryServerArchivist();
+        this.historyServerArchivist = checkNotNull(historyServerArchivist);
 
-        this.executionGraphInfoStore = dispatcherServices.getArchivedExecutionGraphStore();
+        this.executionGraphInfoStore = checkNotNull(executionGraphInfoStore);
 
-        this.jobManagerRunnerFactory = dispatcherServices.getJobManagerRunnerFactory();
+        this.jobManagerRunnerFactory = checkNotNull(jobManagerRunnerFactory);
 
         this.jobManagerRunnerTerminationFutures = new HashMap<>(2);
 
@@ -201,7 +244,7 @@ public abstract class Dispatcher extends PermanentlyFencedRpcEndpoint<Dispatcher
 
         this.dispatcherCachedOperationsHandler =
                 new DispatcherCachedOperationsHandler(
-                        dispatcherServices.getOperationCaches(),
+                        checkNotNull(dispatcherOperationCaches),
                         this::triggerSavepointAndGetLocation,
                         this::stopWithSavepointAndGetLocation);
     }
