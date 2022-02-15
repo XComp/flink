3b111e72bd6f77835826edcf6e807c80eedb18d0
Matthias Pohl <matthias@ververica.com>
2022-02-15 14:00:54 +0100

    [FLINK-25981] Runs test until failure with debug logs being enabled for curator and zookeeper

--- tools/ci/test_controller.sh
+++ tools/ci/test_controller.sh
@@ -115,8 +115,11 @@ else
 	fi
 	MVN_TEST_MODULES=$(get_test_modules_for_stage ${STAGE})
 
-	run_with_watchdog "run_mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE $MVN_TEST_MODULES verify" $CALLBACK_ON_TIMEOUT
-	EXIT_CODE=$?
+  EXIT_CODE=0
+  while [[ $EXIT_CODE == 0 ]]; do
+	  run_with_watchdog "run_mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE -pl flink-runtime verify -Dtest=ZooKeeperMultipleComponentLeaderElectionDriverTest#testLeaderElectionWithMultipleDrivers" $CALLBACK_ON_TIMEOUT
+	  EXIT_CODE=$?
+	done
 fi
 
 # =============================================================================
