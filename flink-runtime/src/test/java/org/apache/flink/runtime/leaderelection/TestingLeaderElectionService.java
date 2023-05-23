920997a40915b7397a9ec84b1b948dff3fe41c33
Matthias Pohl <matthias.pohl@aiven.io>
2023-05-23 09:06:43 +0200

    Adds missing method
    Signed-off-by: Matthias Pohl <matthias.pohl@aiven.io>

--- flink-runtime/src/test/java/org/apache/flink/runtime/leaderelection/TestingLeaderElectionService.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/leaderelection/TestingLeaderElectionService.java
@@ -53,6 +53,10 @@ public class TestingLeaderElectionService implements LeaderElectionService {
         startedLeaderElection.notLeader();
     }
 
+    public synchronized void triggerContenderCleanup() {
+        startedLeaderElection.triggerContenderCleanup();
+    }
+
     public synchronized String getAddress() {
         return getConfirmedLeaderInformation().join().getLeaderAddress();
     }
